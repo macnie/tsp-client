b741e101e6692907b148a35ce855d041efd305ac
macnie <mac@lenmy.com>
2019-06-19 15:15:03 +0800

    增加获取报文、获取轨迹

--- src/tsp/TspClient.php
+++ src/tsp/TspClient.php
@@ -15,7 +15,10 @@
 
 namespace Macnie\Tsp;
 
+use Aliyun\OTS\Consts\DirectionConst;
+use Aliyun\OTS\OTSClient;
 use Curl\Curl;
+use think\facade\Config;
 
 class TspClient
 {
@@ -28,6 +31,118 @@ class TspClient
         if(!isset($this->config['gateway']) || empty($this->config['gateway']) || !isset($this->config['token']) || empty($this->config['token'])){
             throw new \Exception('Configure File Miss Gateway Or Token Param!');
         }
+    }
+
+    /**
+     * 判断设备是否在线
+     * @param string $imei_sn
+     */
+    public function isOnline($imei_sn){
+        $res = $this->get('isOnline',['imei_sn'=>$imei_sn]);
+        return $res['online'];
+    }
+
+    /**
+     * 获取当前合作方的在线设备数量
+     * @return mixed
+     * @throws \Exception
+     */
+    public function getOnlineCount(){
+        $res = $this->get('getOnlineCount');
+        return $res['online_count'];
+    }
+    /**
+     * 获取IMEI当前所有信息
+     * @param string $imei_sn
+     */
+    public function getImei($imei_sn){
+        return $this->get('getImei',['imei_sn'=>$imei_sn]);
+    }
+
+    /**
+     * 获取指定时间内的轨迹
+     * @param string $imei_sn
+     * @param int $start 开始时间戳
+     * @param int $end 结束时间戳
+     * @return array
+     */
+    public function getTracks($imei_sn,$start_time,$end_time){
+
+        $client = new OTSClient([
+            'EndPoint' => $this->config['tablestore']['gateway'],
+            'AccessKeyID' => $this->config['tablestore']['appkey'],
+            'AccessKeySecret' => $this->config['tablestore']['secret'],
+            'InstanceName' => $this->config['tablestore']['database'],
+            'DebugLogHandler'=>false
+        ]);
+        $startPK = [
+            ['imei', md5($imei_sn)],
+            ['created', $start_time]
+        ];
+        $endPK = [
+            ['imei', md5($imei_sn)],
+            ['created',$end_time]
+        ];
+        $tracks = [];
+        while (! empty ($startPK)) {
+            $request = [
+                'table_name' => 'tracks',
+                'max_versions' => 1,
+                'direction' => DirectionConst::CONST_FORWARD, // 方向可以为 FORWARD 或者 BACKWARD
+                'inclusive_start_primary_key' => $startPK, // 开始主键
+                'exclusive_end_primary_key' => $endPK,
+                'limit'=>20
+            ];
+            $response = $client->getRange ($request);
+            foreach ($response['rows'] as $rowData) {
+                // 处理每一行数据
+                $tracks[] = ['direction'=>$rowData['attribute_columns'][3][1],'speed'=>$rowData['attribute_columns'][9][1],'blng'=>$rowData['attribute_columns'][2][1],'blat'=>$rowData['attribute_columns'][1][1],'locate_type'=>$rowData['attribute_columns'][7][1],'locate_time'=>$rowData['attribute_columns'][5][1]];
+            }
+            $startPK = $response['next_start_primary_key'];
+        }
+        return $tracks;
+    }
+
+    /**
+     * 获取终端在某段时间内的报文
+     * @param $imei_sn 设备编号
+     * @param int $start_time 时间戳
+     * @param int $end_time 时间戳
+     * @return array 报文列表
+     */
+    public function getMessages($imei_sn, $start_time, $end_time)
+    {
+        $client = new OTSClient([
+            'EndPoint' => $this->config['tablestore']['gateway'],
+            'AccessKeyID' => $this->config['tablestore']['appkey'],
+            'AccessKeySecret' => $this->config['tablestore']['secret'],
+            'InstanceName' => $this->config['tablestore']['database'],
+            'DebugLogHandler'=>false
+        ]);
+        $startPK = [
+            ['imei', md5($imei_sn)],
+            ['created', $end_time]
+        ];
+        $endPK = [
+            ['imei', md5($imei_sn)],
+            ['created',$start_time]
+        ];
+        $messages = [];
+        while (! empty ($startPK)) {
+            $request = [
+                'table_name' => 'messages',
+                'max_versions' => 1,
+                'direction' => DirectionConst::CONST_BACKWARD, // 方向可以为 FORWARD 或者 BACKWARD
+                'inclusive_start_primary_key' => $startPK, // 开始主键
+                'exclusive_end_primary_key' => $endPK
+            ];
+            $response = $client->getRange ($request);
+            foreach ($response['rows'] as $rowData) {
+                $messages[] = ['message'=>$rowData['attribute_columns'][0][1],'created'=>date('Y-m-d H:i:s',$rowData['primary_key'][1][1])];
+            }
+            $startPK = $response['next_start_primary_key'];
+        }
+        return $messages;
     }
 
     /**
@@ -57,7 +172,7 @@ class TspClient
      * @return mixed
      * @throws \Exception
      */
-    public function setFamily($imei_sn,$families = []){
+    public function setFamilies($imei_sn,$families = []){
         if(empty($families)){
             throw new \Exception('亲情号不能为空');
         }
@@ -107,22 +222,6 @@ class TspClient
         return $this->post('setFind',['imei_sn'=>$imei_sn]);
     }
 
-    /**
-     * 判断设备是否在线
-     * @param string $imei_sn
-     */
-    public function isOnline($imei_sn){
-        $res = $this->get('isOnline',['imei_sn'=>$imei_sn]);
-        return $res['online'];
-    }
-
-    /**
-     * 获取IMEI当前所有信息
-     * @param string $imei_sn
-     */
-    public function getImei($imei_sn){
-        return $this->get('getImei',['imei_sn'=>$imei_sn]);
-    }
     /**
      * 系统设置
      * @param string $imei_sn
